453d6a3a69ffce241de7ce041cf3864e9d3ad8d1
debora soares e silva monteiro <gscmonteiro@gmail.com>
2021-07-16 17:15:32 -0300

    Adiciona perfil do linkedin ao footer e o estiliza

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
 let countdownDate = new Date("Jul 31, 2021 19:00:00").getTime();
 let x = setInterval(function () {
-  // Get today's date and time
   let now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   let distance = countdownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   let days = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("time-days").innerHTML = days + " dias";
   document.getElementById("time-hours").innerHTML = hours + " horas";
   document.getElementById("time-minutes").innerHTML = minutes + " minutos";
   document.getElementById("time-seconds").innerHTML = seconds + " segundos";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
